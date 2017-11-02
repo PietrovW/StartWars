f6e9f32430098f9ac01a0a9ce223cdbd3aa415cc
Piotr Walacik <piotr.walacik@hotmail.com>
2017-11-02 18:15:48 +0100

    Install mvvm light

--- StartWars/StartWars.Android/MainActivity.cs
+++ StartWars/StartWars.Android/MainActivity.cs
@@ -20,7 +20,7 @@ namespace StartWars.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
-            LoadApplication(new App());
+            LoadApplication(new StartWars.App());
         }
     }
 }
